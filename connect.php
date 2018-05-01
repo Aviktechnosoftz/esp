d2c9bd55ea43c5a98f31fd3a305b725f6830714a
neeraj verma <mail.nrj@gmail.com>
2018-05-01 15:13:52 +0530

    db remote connection
    db remote connection

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
 
 error_reporting(0);
-$conn = new mysqli('localhost', 'root', '', "esp");
+$conn = new mysqli('160.153.77.227', 'neerajkeerp', 'v{L0?9^PUfsV', "esp_test");
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
